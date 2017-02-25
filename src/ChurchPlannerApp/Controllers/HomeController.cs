3c2d13f260ecc114b2d09563ad4871ea46aa4990
Patrick Bailey <patrickbailey13@gmail.com>
2017-02-25 15:33:54 -0800

    added home controller with TODO's

---
+++ src/ChurchPlannerApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// TODO: Add Methods to Home Controller to load view based on profile type
+// TODO: Add Controllers and Views to wire up the basic pages of the site
+// TODO: Create Nav functionality
+namespace ChurchPlannerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
