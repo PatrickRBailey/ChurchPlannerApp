1eba374b09a0b471e64c5fe41dbe73456b2e452e
Patrick Bailey <patrickbailey13@gmail.com>
2017-03-01 20:15:49 -0800

    connects to local db and displays messages

---
+++ src/ChurchPlannerApp/Controllers/ForumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChurchPlannerApp.Repositories;
+
+namespace ChurchPlannerApp.Controllers
+{
+    public class ForumController : Controller
+    {
+        private IMessage repository;
+
+        public ForumController(IMessage repo)
+        {
+            repository = repo;
+        }
+        // GET: /<controller>/
+        public ViewResult AllMessages()
+        {
+            return View(repository.GetAllMessages().ToList());
+        }
+    }
+}
