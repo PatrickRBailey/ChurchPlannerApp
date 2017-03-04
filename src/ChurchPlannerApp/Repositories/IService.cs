2106193273988658c6dfe0a934c47df25187a15f
Patrick Bailey <patrickbailey13@gmail.com>
2017-03-04 12:46:23 -0800

    adds Service Repo

---
+++ src/ChurchPlannerApp/Repositories/IService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChurchPlannerApp.Models;
+
+namespace ChurchPlannerApp.Repositories
+{
+    public interface IService
+    {
+        IQueryable<Service> GetAllServices();
+    }
+}
