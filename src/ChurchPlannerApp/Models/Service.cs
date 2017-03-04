462797f51e1c4d879b49cb90390795593c54f987
Patrick Bailey <patrickbailey13@gmail.com>
2017-03-04 12:37:06 -0800

    adds practice date and team to service model

--- src/ChurchPlannerApp/Models/Service.cs
+++ src/ChurchPlannerApp/Models/Service.cs
@@ -8,6 +8,11 @@ namespace ChurchPlannerApp.Models
     public class Service
     {
         public int ServiceID { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime ServiceDate { get; set; }
+        public string Title { get; set; }
+        public DateTime PracticeDate { get; set; }
+
+        private List<Profile> team = new List<Profile>();
+        public List<Profile> Team { get { return team; } }
     }
 }
