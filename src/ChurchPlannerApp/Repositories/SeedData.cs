ba48c3aee5d6d161ca1d72fb959d487f5d608494
Patrick Bailey <patrickbailey13@gmail.com>
2017-03-18 16:00:03 -0700

    fixes seed data

--- src/ChurchPlannerApp/Repositories/SeedData.cs
+++ src/ChurchPlannerApp/Repositories/SeedData.cs
@@ -3,18 +3,51 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using ChurchPlannerApp.Models;
 using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ChurchPlannerApp.Repositories
 {
     public static class SeedData
     {
-        public static void EnsurePopulated(IApplicationBuilder app)
+        public static async Task EnsurePopulated(IApplicationBuilder app)
         {
             ApplicationDBContext context = app.ApplicationServices.GetRequiredService<ApplicationDBContext>();
 
+            string firstName = "Patrick";
+            string lastName = "Bailey";
+            string username = "patrickb";
+            string email = "patrickbailey13@gmail.com";
+            string password = "Fancy123!";
+            string role = "Admin";
+
+            UserManager<MusicUser> userManager = app.ApplicationServices.GetRequiredService<UserManager<MusicUser>>();
+            RoleManager<IdentityRole> roleManager = app.ApplicationServices.GetRequiredService<RoleManager<IdentityRole>>();
+
+
             if (!context.Messages.Any())
             {
-                Profile profile = new Profile { FName = "Johnny", LName = "Rocket" };
+                MusicUser user = await userManager.FindByNameAsync(username);
+                if (user ==null)
+                {
+                    user = new MusicUser { UserName = username, Email = email };
+                    IdentityResult result = await userManager.CreateAsync(user, password);
+
+                    if (await roleManager.FindByNameAsync(role) == null)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole("Admin"));
+                        await roleManager.CreateAsync(new IdentityRole("Musician"));
+                        await roleManager.CreateAsync(new IdentityRole("Leader"));
+
+                        if(result.Succeeded)
+                        {
+                            await userManager.AddToRoleAsync(user, role);
+                        }
+                    }
+                    
+                }
+                Profile profile = new Profile { FName = firstName, LName = lastName, UserName = username, Email = email, User = user };
                 context.Profiles.Add(profile);
 
                 Service service = new Models.Service
@@ -23,21 +56,26 @@ namespace ChurchPlannerApp.Repositories
                     PracticeDate = DateTime.Parse("3/7/2017")
                 ,
                     ServiceDate = DateTime.Parse("3/9/2017")
+                    
                 };
                 context.Services.Add(service);
 
-                //Message message = new Message { Body = "How is everyone doing today?", From = profile };
-                //context.Messages.Add(message);
-                //message = new Message {Body = "Is there practice this week?", From = profile };
-                //context.Messages.Add(message);
+                service = new Models.Service
+                {
+                    Title = "Saturday Night",
+                    PracticeDate = DateTime.Parse("3/11/2017")
+                ,
+                    ServiceDate = DateTime.Parse("3/15/2017")
 
-                //profile = new Profile { FName = "Bob", LName = "Loblaw" };
-                //context.Profiles.Add(profile);
+                };
 
-                //message = new Message {Body = "Can anyone play this week?", From = profile };
-                //context.Messages.Add(message);
-                //message = new Message { Body = "This site is so fun!!", From = profile };
-                //context.Messages.Add(message);
+                context.Services.Add(service);
+
+                Message message = new Message { Body = "Is there practice this week?", From = profile };
+                Comment comment = new Comment { Body = "Yep", From = profile };
+                message.Comments.Add(comment);
+                context.Messages.Add(message);
+
 
                 Song song = new Song { Title = "How Great Is Our God", Url = "https://youtu.be/KBD18rsVJHk" };
                 context.Songs.Add(song);
