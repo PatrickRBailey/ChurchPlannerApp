40d65d3f4ed68783728ff757a6414aa85744bddd
Patrick Bailey <patrickbailey13@gmail.com>
2017-03-19 23:17:32 -0700

    hotfix

--- src/ChurchPlannerApp/Repositories/ApplicationDBContext.cs
+++ src/ChurchPlannerApp/Repositories/ApplicationDBContext.cs
@@ -17,7 +17,6 @@ namespace ChurchPlannerApp.Repositories
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<Song> Songs { get; set; }
         public DbSet<Service> Services { get; set; }
-        public DbSet<Instrument> Instruments { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ServiceRequest>()
